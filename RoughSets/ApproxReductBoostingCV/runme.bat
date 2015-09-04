66a735a49e8c31529c340d98ec5459e544d80d71
Sebastian Widz <widz@infovision.pl>
2015-09-04 23:56:44 +0000

    201504061918
    Originally checked in by Hujolopter\Sebastian on 2015-04-06 19:18:18.403 as Changeset 114
    
    git-tfs-id: [https://infovision.visualstudio.com/DefaultCollection]$/RoughSets;C150

--- RoughSets/ApproxReductBoostingCV/runme.bat
+++ RoughSets/ApproxReductBoostingCV/runme.bat
@@ -1,4 +1,4 @@
-start "chess" /i /min /low ApproxReductBoostingCV.exe Data\chess.dta 20 100 1 1 | wtee chess.txt
+start "chess" /i /min /low ApproxReductBoostingCV.exe Data\chess.data 20 100 1 1 | wtee chess.txt
 start "semeion" /i /min /low ApproxReductBoostingCV.exe Data\semeion.data 20 100 1 1 | wtee semeion.txt
 start "zoo" /i /min /low ApproxReductBoostingCV.exe Data\zoo.data 20 100 1 1 | wtee zoo.txt
 start "soybean-small" /i /min /low ApproxReductBoostingCV.exe Data\soybean-small.2.data 20 100 1 1 | wtee soybean-small.txt

7433ab94c1de9b20c7e8db49ae3cb74cb8b5990b
Sebastian Widz <widz@infovision.pl>
2015-09-04 23:42:19 +0000

    201409131917
    Originally checked in by Hujolopter\Sebastian on 2014-09-13 19:17:36.44 as Changeset 31
    
    git-tfs-id: [https://infovision.visualstudio.com/DefaultCollection]$/RoughSets;C67

---
+++ RoughSets/Infovision.DataVisualization.Tests/GDITestClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace Infovision.DataVisualization.Tests
+{
+    [TestFixture]
+    public class GDITestClass
+    {
+        [Test]
+        public void MethodTest()
+        {
+            int width = 640;
+            int height = 380;
+            System.Drawing.Imaging.PixelFormat pixelFormat = System.Drawing.Imaging.PixelFormat.Format32bppArgb;
+
+            Bitmap bitmap = new Bitmap(width, height, pixelFormat);
+            Graphics g = Graphics.FromImage(bitmap);            
+        }
+    }
+}

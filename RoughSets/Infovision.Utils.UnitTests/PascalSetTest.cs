33a8204265e62ff5415ca5b9563ec7a304c0efa1
Sebastian Widz <widz@infovision.pl>
2016-09-04 22:08:30 +0000

    Update NuGet Packages
    git-tfs-id: [https://infovision.visualstudio.com/DefaultCollection]$/RoughSets;C489

--- RoughSets/Infovision.Utils.UnitTests/PascalSetTest.cs
+++ RoughSets/Infovision.Utils.UnitTests/PascalSetTest.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Infovision.Utils.UnitTests
 {
@@ -26,6 +29,43 @@ namespace Infovision.Utils.UnitTests
 
             set3 = set1.Intersection(set2);
             Assert.AreEqual(5, set3.GetCardinality());
+        }
+
+        [Test]
+        public void HashSetTest()
+        {
+            Stopwatch s1 = new Stopwatch();
+            s1.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                PascalSet<long> set1 = new PascalSet<long>(0, 10, new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+                PascalSet<long> set2 = new PascalSet<long>(0, 10, new long[] { 5, 6, 7, 8, 9 });
+
+                //PascalSet<int> union = set1 + set2;
+                //PascalSet<int> intersection = set1 * set2;
+                //PascalSet<int> difference = set1 - set2;
+                bool isSuperSet = set1.Superset(set2);
+            }
+            s1.Stop();
+            Console.WriteLine("Pascal {0}", s1.ElapsedMilliseconds);
+
+
+            Stopwatch s2 = new Stopwatch();
+            s2.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                HashSet<long> set3 = new HashSet<long>(new long[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+                HashSet<long> set4 = new HashSet<long>(new long[] { 5, 6, 7, 8, 9 });
+
+                //set3.UnionWith(set4);
+                //set4.IntersectWith(set3);
+                //set3.ExceptWith(set4);
+
+                bool isSuperSet = set3.IsSupersetOf(set3);
+            }
+            s2.Stop();
+            Console.WriteLine("HashSet {0}", s2.ElapsedMilliseconds);
+
         }
     }
 }

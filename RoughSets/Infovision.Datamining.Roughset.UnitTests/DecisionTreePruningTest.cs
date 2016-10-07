4868b08cbb818c07b92f2e14fcae59fa25ed08f0
Sebastian Widz <widz@infovision.pl>
2016-10-07 09:08:03 +0000

    DecisionTreePrunningTest changes
    git-tfs-id: [https://infovision.visualstudio.com/DefaultCollection]$/RoughSets;C507

--- RoughSets/Infovision.Datamining.Roughset.UnitTests/DecisionTreePruningTest.cs
+++ RoughSets/Infovision.Datamining.Roughset.UnitTests/DecisionTreePruningTest.cs
@@ -28,28 +28,43 @@ namespace Infovision.Datamining.Roughset.UnitTests
 
             ClassificationResult resultWithoutPruning = Classifier.DefaultClassifer.Classify(c45WithoutPruning, test);
             Console.WriteLine("resultWithoutPruning =  {0}", resultWithoutPruning);
+            Console.WriteLine("number of rules: {0}", this.GetNumberOfRules(c45WithoutPruning));
+
+            //===============================================
 
             DecisionTreeC45 c45WithPruning = new DecisionTreeC45();
             c45WithPruning.Learn(train, train.DataStoreInfo.GetFieldIds(FieldTypes.Standard).ToArray());
 
             ClassificationResult resultBeforePruning = Classifier.DefaultClassifer.Classify(c45WithPruning, test);
             Console.WriteLine("resultBeforePruning = {0}", resultBeforePruning);
+            Console.WriteLine("number of rules: {0}", this.GetNumberOfRules(c45WithPruning));
 
             ErrorBasedPruning pruning = new ErrorBasedPruning(c45WithPruning, prune);
-            pruning.Threshold = 0.05;
+            pruning.Threshold = 0.1;
             pruning.Run();
             
             ClassificationResult resultAfterPruning = Classifier.DefaultClassifer.Classify(c45WithPruning, test);
             Console.WriteLine("resultAfterPruning = {0}", resultAfterPruning);
+            Console.WriteLine("number of rules: {0}", this.GetNumberOfRules(c45WithPruning));
+
+            //===============================================
 
             DecisionTreeC45 c45WithPrePruning = new DecisionTreeC45();
-            c45WithPrePruning.Epsilon = 0.05;
+            c45WithPrePruning.Epsilon = 0.1;
             c45WithPrePruning.Learn(data, data.DataStoreInfo.GetFieldIds(FieldTypes.Standard).ToArray());
 
             ClassificationResult resultForTreeWithPrePruning = Classifier.DefaultClassifer.Classify(c45WithPrePruning, test);
             Console.WriteLine("resultForTreeWithPrePruning = {0}", resultForTreeWithPrePruning);
+            Console.WriteLine("number of rules: {0}", this.GetNumberOfRules(c45WithPrePruning));
 
             Console.WriteLine();
+        }
+
+        public int GetNumberOfRules(IDecisionTree tree)
+        {
+            int count = 0;
+            TreeNodeTraversal.TraversePostOrder(tree.Root, x => count = x.IsLeaf ? count + 1 : count);
+            return count;
         }
     }
 }

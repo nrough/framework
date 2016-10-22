b76bfd7aeeceed1886cc1b9c1ed86f334f98154e
Sebastian Widz <widz@infovision.pl>
2016-10-22 20:15:08 +0000

    Obilivious Decision Trees Unit Testing
    git-tfs-id: [https://infovision.visualstudio.com/DefaultCollection]$/RoughSets;C525

--- RoughSets/Infovision.Datamining.Roughset.UnitTests/ObiliviousDecisionTreeTest.cs
+++ RoughSets/Infovision.Datamining.Roughset.UnitTests/ObiliviousDecisionTreeTest.cs
@@ -15,15 +15,20 @@ namespace Infovision.Datamining.Roughset.UnitTests.DecisionTrees
     public class ObiliviousDecisionTreeTest
     {
         [Test]
-        //[TestCase(@"Data\monks-1.train", @"Data\monks-1.test")]
+        [TestCase(@"Data\spect.train", @"Data\spect.test")]
+        [TestCase(@"Data\monks-1.train", @"Data\monks-1.test")]
+        [TestCase(@"Data\monks-2.train", @"Data\monks-2.test")]
+        [TestCase(@"Data\monks-3.train", @"Data\monks-3.test")]
         [TestCase(@"Data\dna_modified.trn", @"Data\dna_modified.tst")]
         public void LearnTest(string trainFile, string testFile)
         {
             double epsilon = -1;
             int numOfReducts = 100;
-            double reductEpsilon = 0.0;
+            double reductEpsilon = 0.01;
 
             //Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+            Console.WriteLine("Obilivious Decision Tree");
 
             DataStore data = DataStore.Load(trainFile, FileFormat.Rses1);
             foreach (var fieldInfo in data.DataStoreInfo.Fields) fieldInfo.IsNumeric = false;
@@ -37,12 +42,17 @@ namespace Infovision.Datamining.Roughset.UnitTests.DecisionTrees
             Console.WriteLine(Classifier.DefaultClassifer.Classify(obiliviousTree, test));
             //Console.WriteLine(DecisionTreeFormatter.Construct(obiliviousTree));
 
+            Console.WriteLine("Obilivious Decision Tree with Pruning");
+
             ErrorBasedPruning pruning = new ErrorBasedPruning(obiliviousTree, test);
             //ReducedErrorPruning pruning = new ReducedErrorPruning(obiliviousTree, test);
             pruning.Prune();
 
             Console.WriteLine(Classifier.DefaultClassifer.Classify(obiliviousTree, data));
             Console.WriteLine(Classifier.DefaultClassifer.Classify(obiliviousTree, test));
+
+
+            Console.WriteLine("M Decision Tree based on Reduct");
 
             DecisionTreeReduct treeRed = new DecisionTreeReduct();
             if (epsilon >= 0)
@@ -56,11 +66,15 @@ namespace Infovision.Datamining.Roughset.UnitTests.DecisionTrees
 
             IReduct reduct = treeRed.Reduct;
 
+            Console.WriteLine("Obilivious Decision Tree based on Reduct");
+
             ObliviousDecisionTree obiliviousTree2 = new ObliviousDecisionTree();
             if (epsilon >= 0)
                 obiliviousTree2.Epsilon = epsilon;
             Console.WriteLine(obiliviousTree2.Learn(data, reduct.Attributes.ToArray()));
             Console.WriteLine(Classifier.DefaultClassifer.Classify(obiliviousTree2, test));
+
+            Console.WriteLine("Obilivious Decision Tree based on Reduct after Pruning");
 
             ErrorBasedPruning pruning2 = new ErrorBasedPruning(obiliviousTree2, test);
             //ReducedErrorPruning pruning = new ReducedErrorPruning(obiliviousTree, test);
@@ -68,6 +82,26 @@ namespace Infovision.Datamining.Roughset.UnitTests.DecisionTrees
 
             Console.WriteLine(Classifier.DefaultClassifer.Classify(obiliviousTree2, data));
             Console.WriteLine(Classifier.DefaultClassifer.Classify(obiliviousTree2, test));
+
+            Console.WriteLine("C45 Decision Tree");
+
+            DecisionTreeC45 c45 = new DecisionTreeC45();
+            if (epsilon >= 0)
+                c45.Epsilon = epsilon;
+            Console.WriteLine(c45.Learn(data, attributes));
+            Console.WriteLine(Classifier.DefaultClassifer.Classify(c45, test));
+
+            Console.WriteLine("C45 Decision Tree with Prunning");
+
+            DecisionTreeC45 c45p = new DecisionTreeC45();
+            c45p.Pruning = true;
+            c45p.PruningObjective = PruningObjectiveType.MinimizeError;
+            if (epsilon >= 0)
+                c45p.Epsilon = epsilon;
+            Console.WriteLine(c45p.Learn(data, attributes));
+            Console.WriteLine(Classifier.DefaultClassifer.Classify(c45p, test));
+
+            
         }
     }
 }

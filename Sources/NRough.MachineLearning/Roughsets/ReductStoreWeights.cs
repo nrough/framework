061adf55f6739dd6f7bee73c4534b2eb996c188c
NRough Framework <authors@nrough.net>
2017-05-07 23:33:15 +0200

    Correct a typo

--- Sources/NRough.MachineLearning/Roughsets/ReductStoreWeights.cs
+++ Sources/NRough.MachineLearning/Roughsets/ReductStoreWeights.cs
@@ -2,7 +2,7 @@
 {
     public class ReductStoreWeights : ReductStore
     {
-        //TODO beeing super set is OK, but w must be different
+        //TODO being super set is OK, but w must be different
         protected override bool CanAddReduct(IReduct reduct)
         {
             if (this.IsSuperSet(reduct))

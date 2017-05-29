b5a00c08cce7383d7bcecd083ae575b4b894eb39
swidz <widz@infovision.pl>
2017-05-29 22:07:33 +0200

    Remove wtee.exe from the solution and Fix SimpleITK reference

--- Experiments/ApproxReductBoosting/runme.bat
+++ Experiments/ApproxReductBoosting/runme.bat
@@ -1,10 +1,10 @@
-ApproxReductBoosting.exe Data\soybean-large.data Data\soybean-large.test 20 100 1 1 1 1 | wtee soybean-large.txt
-ApproxReductBoosting.exe Data\dna_modified.trn Data\dna_modified.tst 20 100 1 1 | wtee dna_modified.txt
-ApproxReductBoosting.exe Data\monks-1.train Data\monks-1.test 20 100 1 1 | wtee monks-1.txt
-ApproxReductBoosting.exe Data\monks-2.train Data\monks-2.test 20 100 1 1 | wtee monks-2.txt
-ApproxReductBoosting.exe Data\monks-3.train Data\monks-3.test 20 100 1 1 | wtee monks-3.txt
-ApproxReductBoosting.exe Data\SPECT.train Data\SPECT.test 20 100 1 1 | wtee spect.txt
-ApproxReductBoosting.exe Data\audiology.standardized.2.data Data\audiology.standardized.2.test 20 100 1 1 | wtee audiology.standarized.2.txt
-ApproxReductBoosting.exe Data\pendigits.trn Data\pendigits.tst 20 100 1 1 | wtee pendigit.txt
-ApproxReductBoosting.exe Data\optdigits.trn Data\optdigits.tst 20 100 1 1 | wtee optdigit.txt
-ApproxReductBoosting.exe Data\letter.trn Data\letter.tst 20 100 1 1 | wtee letter.txt
+ApproxReductBoosting.exe Data\soybean-large.data Data\soybean-large.test 20 100 1 1 1 1 
+ApproxReductBoosting.exe Data\dna_modified.trn Data\dna_modified.tst 20 100 1 1 
+ApproxReductBoosting.exe Data\monks-1.train Data\monks-1.test 20 100 1 1
+ApproxReductBoosting.exe Data\monks-2.train Data\monks-2.test 20 100 1 1
+ApproxReductBoosting.exe Data\monks-3.train Data\monks-3.test 20 100 1 1
+ApproxReductBoosting.exe Data\SPECT.train Data\SPECT.test 20 100 1 1
+ApproxReductBoosting.exe Data\audiology.standardized.2.data Data\audiology.standardized.2.test 20 100 1 1
+ApproxReductBoosting.exe Data\pendigits.trn Data\pendigits.tst 20 100 1 1
+ApproxReductBoosting.exe Data\optdigits.trn Data\optdigits.tst 20 100 1 1
+ApproxReductBoosting.exe Data\letter.trn Data\letter.tst 20 100 1 1

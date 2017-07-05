6f93b6ad3693a3de68a1e5cd66b77cae567e3729
authors@nrough.net <Axapta!@#123>
2017-07-05 09:52:29 +0200

    Fixed license header

--- Tests/NRough.Tests.MachineLearning/Classification/DecisionTrees/ObliviousDecisionTreeTest.cs
+++ Tests/NRough.Tests.MachineLearning/Classification/DecisionTrees/ObliviousDecisionTreeTest.cs
@@ -1,27 +1,4 @@
-﻿//
-// NRough Framework
-// http://www.nrough.net
-//
-// Copyright © Sebastian Widz, 2017
-// authors at nrough.net
-
-//This file is part of NRough.
-
-//Foobar is free software; you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation; either version 3 of the License, or
-//(at your option) any later version.
-//
-//Foobar is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with Foobar; if not, write to the Free Software
-//Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-using NRough.Data;
+﻿using NRough.Data;
 using NRough.MachineLearning.Classification.DecisionTrees;
 using NRough.MachineLearning.Classification.DecisionTrees.Pruning;
 using NRough.Core;
